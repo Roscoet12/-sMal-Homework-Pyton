7c1d1c44e9d328b79d96128ea97303e8beac5e94
Roscoe <kseshamal74@gmail.com>
2025-11-06 21:34:18 +0300

    1 урок

---
+++ lesson_01/lesson_1_task_3.py
@@ -0,0 +1,3 @@
+first_name = input("Введите имя: ")
+last_name = input("Введите фамилию: ")
+print("Вас зовут " + first_name, end=" " + last_name)
---
+++ lesson_01/lesson_1_task_5.py
@@ -0,0 +1,15 @@
+def printNum(num):
+    print(num, end="")
+
+
+printNum(8)
+printNum(8)
+printNum(0)
+printNum(0)
+printNum(5)
+printNum(5)
+printNum(5)
+printNum(3)
+printNum(5)
+printNum(3)
+printNum(5)
