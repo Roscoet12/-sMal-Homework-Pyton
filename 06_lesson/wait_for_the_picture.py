69c068845ea759d31890943e8cb8b006da5c7674
Roscoe <kseshamal74@gmail.com>
2025-12-18 15:46:10 +0300

    6.1 урок

--- 06_lesson/wait_for_the_picture.py
+++ 06_lesson/wait_for_the_picture.py
@@ -9,11 +9,12 @@ from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 driver.get('https://bonigarcia.dev/selenium-webdriver-java/loading-images.html')
-
 weiter = WebDriverWait(driver, 20)
-weiter.until(
-    EC.visibility_of_element_located((By.CSS_SELECTOR, '#landscape'))
-)
+for img in ['#compass', '#calendar', '#award', '#landscape']:
+    weiter.until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, img))
+   )
+
 img_three = driver.find_element(By.CSS_SELECTOR, '#award').get_attribute('src')
 print(img_three)
 
