ca845b1d44c4c9a6eb1c11374cd5ab26c7515548
Roscoe <kseshamal74@gmail.com>
2026-01-28 13:31:37 +0300

    9.2

--- 08_lesson/Yougile.py
+++ 08_lesson/Yougile.py
@@ -3,18 +3,18 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+
 class Yougile:
     def __init__(self, url):
         self.url = url
 
         login = os.getenv("LOGIN")
-        password = os.getenv("PASSWORD")
-
         self.login = login
 
+        password = os.getenv("PASSWORD")
         self.password = password
 
-        name = "Sweettooth"
+        name = os.getenv("COMPANY_NAME")
         self.name = name
 
         self.company_id = self.get_id_company(login, password, name)
@@ -22,7 +22,7 @@ class Yougile:
         self.token = self.get_token(login, password, self.company_id)
 
     def get_id_company(self, login, password, name):
-        creds ={
+        creds = {
             'login': login,
             'password': password,
             'name': name
@@ -36,7 +36,7 @@ class Yougile:
             'password': password,
             'companyId': companyId
         }
-        resp =requests.post(self.url + '/auth/keys', json=creds)
+        resp = requests.post(self.url + '/auth/keys', json=creds)
         return resp.json()['key']
 
     def get_id_employee(self):
@@ -45,8 +45,6 @@ class Yougile:
         }
 
         resp = requests.get(self.url + '/users', headers=headers)
-        response_json = resp.json()
-        print(response_json)
         return resp.json()['content'][0]['id']
 
     def get_new_project(self, title, users):
@@ -57,7 +55,8 @@ class Yougile:
             'title': title,
             'users': users
         }
-        resp = requests.post(self.url + '/projects', json=project,headers=headers)
+        resp = requests.post(self.url + '/projects',
+                             json=project, headers=headers)
         return resp
 
     def get_project(self):
@@ -71,7 +70,8 @@ class Yougile:
         headers = {
             'Authorization': f'Bearer {self.token}'
         }
-        resp = requests.get(self.url + '/projects/' + id_project, headers=headers)
+        resp = requests.get(self.url + '/projects/' + id_project,
+                            headers=headers)
         return resp
 
     def put_project(self, id_project, name_project):
@@ -82,8 +82,6 @@ class Yougile:
             'deleted': True,
             'title': name_project
         }
-        resp = requests.put(self.url + '/projects/' + id_project, json=project, headers=headers)
+        resp = requests.put(self.url + '/projects/' + id_project,
+                            json=project, headers=headers)
         return resp
-
-    def delete_key(self):
-        resp = requests.delete(self.url + '/auth/keys/'+ self.token)
--- 08_lesson/test_yougile.py
+++ 08_lesson/test_yougile.py
@@ -1,9 +1,10 @@
 from Yougile import Yougile
 api = Yougile('https://ru.yougile.com/api-v2')
 
-def test_new_project_positiv():
 
-    users_id  = api.get_id_employee()
+def test_new_project_positive():
+    users_id = api.get_id_employee()
+
     users = {
          users_id: 'admin',
     }
@@ -12,13 +13,14 @@ def test_new_project_positiv():
     assert result.status_code == 201
 
     project_list = api.get_project()
+    projects = project_list.get('content', [])
+    my_project = [proj for proj in projects
+                  if proj.get('title') == 'Сладкоежка']
 
-    assert project_list['content'][0]['title'] == 'Сладкоежка'
+    assert my_project, 'Проект "Сладкоежка" не найден в списке'
 
-    api.delete_key()
 
 def test_new_project_negativ():
-
     users = {
         12345: 'admin',
     }
@@ -26,42 +28,72 @@ def test_new_project_negativ():
 
     assert result.status_code == 400
 
-    api.delete_key()
 
-def test_get_project_id_positiv():
+def test_get_project_id_positive():
+    users_id = api.get_id_employee()
+    users = {
+        users_id: 'admin',
+    }
+    api.get_new_project('Сладкоежка', users)
 
-    result = api.get_project()
-    id_project = result['content'][0]['id']
+    project_list = api.get_project()
+    assert ('content' in project_list
+            and len(project_list['content']) > 0), 'Нет проектов в ответе'
+    projects = project_list['content']
+    my_project = None
+    for proj in projects:
+        if proj['title'] == 'Сладкоежка':
+            my_project = proj
+            break
 
-    result2 = api.get_project_id(id_project)
+    assert my_project is not None, 'Проект "Сладкоежка" не найден в списке'
 
-    assert result2.status_code == 200
-    assert result['content'][0]['title'] == 'Сладкоежка'
+    id_project = my_project['id']
 
-    api.delete_key()
+    result = api.get_project_id(id_project)
+
+    assert result.status_code == 200
+
 
 def test_get_project_id_negativ():
-
     id_project = '12345'
     result = api.get_project_id(id_project)
 
     assert result.status_code == 404
 
-    api.delete_key()
 
-def test_change_positiv():
+def test_change_positive():
+    users_id = api.get_id_employee()
+    users = {
+        users_id: 'admin',
+    }
+    api.get_new_project('Сладкоежка', users)
 
-    result = api.get_project()
-    id_project = result['content'][0]['id']
-    name_project = result['content'][0]['title']
+    project_list = api.get_project()
+    projects = project_list['content']
+    my_project = None
+    for proj in projects:
+        if proj['title'] == 'Сладкоежка':
+            my_project = proj
+            break
+
+    id_project = my_project['id']
+    name_project = my_project['title']
 
     change_project = api.put_project(id_project, name_project)
 
-    projects = api.get_project()
-    assert projects['content'] == []
     assert change_project.status_code == 200
 
-    api.delete_key()
+    project_list_change = api.get_project()
+    projects_change = project_list_change['content']
+    no_project = None
+    for proj in projects_change:
+        if proj['title'] == 'Сладкоежка':
+            no_project = proj
+            break
+
+    assert no_project is None, 'Проект "Сладкоежка" не удален'
+
 
 def test_change_project_negativ():
     id_project = '234'
@@ -69,5 +101,3 @@ def test_change_project_negativ():
     change_project = api.put_project(id_project, name_project)
 
     assert change_project.status_code == 404
-
-    api.delete_key()
